d6d8d22b6771f4d00a3e0a4fdb5c49fd884a181b
m-anan <mohmad2000.an@gmail.com>
2025-10-25 20:28:46 +0400

    update siemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,27 +1,10 @@
 import { MetadataRoute } from "next";
 import useGlobalService from "@/core/hook/useGlobalService";
-import ProductService from "@/modules/products/services/ProductService";
 
 const rootUrl = `${process.env.NEXT_PUBLIC_URL}`;
-async function fetchSlugs() {
-  const productService = useGlobalService(ProductService);
-
-  const Products = await productService.methods.getProducts({});
-  const slugs = Products.map((data) => `${data.id}`);
-  return slugs;
-}
+//
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const slugs = await fetchSlugs();
-
-  const productsListing: MetadataRoute.Sitemap = slugs.map((slug) => {
-    return {
-      url: `${rootUrl}/products/${slug}`,
-      lastModified: new Date(),
-      changeFrequency: "always",
-    };
-  });
-
   return [
     {
       url: rootUrl,
@@ -29,19 +12,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: "yearly",
       priority: 1,
     },
-    {
-      url: `${rootUrl}/cart`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${rootUrl}/login`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-
-    ...productsListing,
   ];
 }
